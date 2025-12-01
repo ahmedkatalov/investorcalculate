17041d8c303e0d792c281189755b9c46937ca4ac
Akhmed <aaaakk122123@gmail.com>
2025-12-01 22:10:04 +0300

    ак

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,4 +1,5 @@
-export const API_URL = import.meta.env.VITE_API_URL; // "/api"
+export const API_URL = import.meta.env.VITE_API_URL || "/api";
+
 
 function authHeaders() {
   const token = localStorage.getItem("token");
